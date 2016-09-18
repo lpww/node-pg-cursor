9c48f36669694c91c9adb082a9e8b79ce1b6ed42
lpww <t.treffry@gmail.com>
2016-09-17 17:45:06 -0700

    Use resolve package in place of require.resolve function

--- pg.js
+++ pg.js
@@ -1,13 +1,14 @@
-var path = require('path')
+var path = require('path');
+var resolve = require('resolve');
 var pgPath;
 //support both pg & pg.js
 //this will eventually go away when i break native bindings
 //out into their own module
 try {
-  pgPath = path.dirname(require.resolve('pg'))
+  pgPath = path.dirname(resolve.sync('pg', {basedir: __dirname}));
 } catch(e) {
-  pgPath = path.dirname(require.resolve('pg.js')) + '/lib'
+  pgPath = path.dirname(resolve.sync('pg.js', {basedir: __dirname})) + '/lib';
 }
 
-module.exports.Result = require(path.join(pgPath, 'result.js'))
-module.exports.prepareValue = require(path.join(pgPath, 'utils.js')).prepareValue
+module.exports.Result = require(path.join(pgPath, 'result.js'));
+module.exports.prepareValue = require(path.join(pgPath, 'utils.js')).prepareValue;
